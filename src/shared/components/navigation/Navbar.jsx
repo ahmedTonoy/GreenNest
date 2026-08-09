d8e0531cca82469bc25f0aad30cf0179bf2b42e8
ahmedTonoy <forkan1510699@gmail.com>
2026-08-09 23:12:50 +0600

    fix(navbar): change z index value for navbar menu icon for small device visibility

--- src/shared/components/navigation/Navbar.jsx
+++ src/shared/components/navigation/Navbar.jsx
@@ -35,7 +35,7 @@ const Navbar = () => {
             </div>
             <ul
               tabIndex="-1"
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-30 p-2 shadow"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-10 mt-3 w-30 p-2 shadow"
             >
               {links}
             </ul>
